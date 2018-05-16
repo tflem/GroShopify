eb3dd208fe51f43071b106e358cfe705d7ac771f
Tim Fleming <tfleming26@hotmail.com>
2018-05-16 09:50:03 -0600

    Add product class

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+namespace GroShopify.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }        
+    }
+}
