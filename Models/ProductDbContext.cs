e51cd5d28cc5c6de7341757f86da50db85fd5f65
Tim Fleming <tfleming26@hotmail.com>
2018-05-16 11:56:37 -0600

    Add product db context class

---
+++ Models/ProductDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Entensions.DependencyInjection;
+
+namespace GroShopify.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+           : base(options) { }
+
+        public DbSet<Product> Products {get; set;}        
+    }
+}
