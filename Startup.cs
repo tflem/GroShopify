05234cc030f60b4ab2700c7bebaede74332a7aac
Tim Fleming <tfleming26@hotmail.com>
2018-05-16 11:07:06 -0600

    Setup default route

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,10 @@ namespace GroShopify
             app.UseStaticFiles();
             app.UseMvc(routes =>
             {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Product}/{action=List}/{id?}");
+                )
 
             });            
         }
