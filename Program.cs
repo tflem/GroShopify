136906546a62441b711937538000cc5994f0cf14
Tim Fleming <tfleming26@hotmail.com>
2018-05-16 12:35:12 -0600

    Disable scope verification

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ namespace GroShopify
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseDefaultServiceProvider(options =>
+                    options.ValidateScopes = false)
                 .Build();
     }
 }
