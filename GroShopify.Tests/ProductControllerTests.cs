8f0ee99ccfe53700b5498520dbce41a197fd402d
Tim Fleming <tfleming26@hotmail.com>
2018-05-20 14:03:33 -0600

    Update can paginate test and add pagination view model test

--- GroShopify.Tests/ProductControllerTests.cs
+++ GroShopify.Tests/ProductControllerTests.cs
@@ -14,27 +14,57 @@ namespace GroShopify.Tests
         {
             // Arrange
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
-                mock.Setup(m => m.Products).Returns((new Product[]
-                {
-                    new Product { ProductID = 1, Name = "P1" },
-                    new Product { ProductID = 2, Name = "P2" },
-                    new Product { ProductID = 3, Name = "P3" },
-                    new Product { ProductID = 4, Name = "P4" },
-                    new Product { ProductID = 5, Name = "P5" }
-                }).AsQueryable<Product>());
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+                new Product { ProductID = 4, Name = "P4" },
+                new Product { ProductID = 5, Name = "P5" }
+            }).AsQueryable<Product>());
 
-                ProductController controller = new ProductController(mock.Object);
-                controller.PageSize = 3;
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
 
             // Act
-            IEnumerable<Product> result =
-                controller.List(2).ViewData.Model as IEnumerable<Product>;
+            ProductListViewModel result =
+                controller.List(2).ViewData.Model as ProductListViewModel;
 
             // Assert
-            Product[] productArray = result.ToArray();
+            Product[] productArray = result.Products.ToArray();
             Assert.True(productArray.Length == 2);
             Assert.Equal("P4", productArray[0].Name);
             Assert.Equal("P5", productArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>():
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1"},
+                new Product { ProductID = 2, Name = "P2"},
+                new Product { ProductID = 3, Name = "P3"},
+                new Product { ProductID = 4, Name = "P4"},
+                new Product { ProductID = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+
+            // Arrange
+            ProductController controller = 
+                new ProductController(mock.Object) { PageSize = 3 };
+
+            // Act
+            ProductListViewModel result =
+                controller.List(2).ViewData.Model as ProductListViewModel;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
         }
     }    
 }
