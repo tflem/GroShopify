16315aa423efc44770faeebacc459bf67d8d9ee9
Tim Fleming <tfleming26@hotmail.com>
2018-05-21 22:26:27 -0600

    Add classes to generated elements

--- GroShopify/Infrastructure/PageLinkTagHelper.cs
+++ GroShopify/Infrastructure/PageLinkTagHelper.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using GroShopify.Models.ViewModels;
 
+
 namespace GroShopify.Infrastructure
 {
     [HtmlTargetElement("div", Attributes = "page-model")]
@@ -25,20 +26,30 @@ namespace GroShopify.Infrastructure
 
         public string PageAction { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;              
+        public string PageClass { get; set; }              
+        public string PageClassNormal { get; set; }              
+        public string PageClassSelected { get; set; }    
+
         public override void Process(TagHelperContext context,
                 TagHelperOutput output) 
         {
-                IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
-                TagBuilder result = new TagBuilder("div");
-                for (int i = 1; i <= PageModel.TotalPages; i++)
-                {
-                    TagBuilder tag = new TagBuilder("a");
-                    tag.Attributes["href"] = urlHelper.Action(PageAction,
-                       new { productPage = 1 });
-                    tag.InnerHtml.Append(i.ToString());
-                    result.InnerHtml.AppendHtml(tag);
-                }
-                output.Content.AppendHtml(result.InnerHtml);
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction,
+                    new { productPage = i });
+                if (PageClassesEnabled) {              
+                    tag.AddCssClass(PageClass);              
+                    tag.AddCssClass(i == PageModel.CurrentPage              
+                        ? PageClassSelected : PageClassNormal);              
+                }  
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+            output.Content.AppendHtml(result.InnerHtml);
         }              
     }
 }
